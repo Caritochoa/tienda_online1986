34f7c286287ddd2367a228c863c6f941c4fc0880
Carolina <karittochoa@gmail.com>
2018-12-03 16:23:20 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Buscador from "./componentes/Buscador";
+import Resultado from "./componentes/Resultado";
 
 class App extends Component {
+  state = {
+    termino: "",
+    imagenes: []
+  };
+
+  consultarApi = () => {
+    const url = `https://pixabay.com/api/videos/?key=10885245-c6440a9fde04a4b323cd16c61=${
+      this.state.termino
+    }`;
+
+    fetch(url)
+      .then(respuesta => respuesta.json())
+      .then(resultado => this.setState({ imagenes: resultado.hits }));
+  };
+
+  datosBusqueda = termino => {
+    this.setState(
+      {
+        termino
+      },
+      () => {
+        this.consultarApi();
+      }
+    );
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App container">
+        <div className="jumbotron">
+          <p className="lead text-center">Buscador de Imagenes</p>
+          <Buscador datosBusqueda={this.datosBusqueda} />
+        </div>
+        <Resultado imagenes={this.state.imagenes} />
+        {this.state.termino}
       </div>
     );
   }
